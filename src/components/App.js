a7f6d0095452d6f8eaa85278b5596beb793aa8b4
tusharsharma000 <84301919+tusharsharma000@users.noreply.github.com>
2022-02-25 14:47:44 +0530

    string

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../styles/App.css";
 
-function App() 
-{
-	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
-}
+function App() {
+  const [inputs, setInputs] = useState("");
+	const [items, setItems] = useState([]);
+	const [toggle, setToggle] = useState(true);
+	const [isEditItem , setIsEditItem] = useState(null);
 
+
+  const heandlechange = (e) => {
+    setInputs(e.target.value);
+  };
+  const additem = () => {
+    if (!inputs) {
+
+		} 
+		else if(inputs && !toggle){
+     setItems(items.map((elem)=>{
+     if(elem.id === isEditItem){
+			 return {...elem,name:inputs}
+		 }
+		 return elem;
+		 }))
+		 setToggle(true);
+			setInputs('');
+			setIsEditItem(null);
+		}
+		else {
+      const allitems = { id: new Date().getTime().toString(), name: inputs };
+      setItems([...items, allitems]);
+      setInputs("");
+    }
+  };
+
+  const del = (index) => {
+    const updateditem = items.filter((elem) => {
+      return index !== elem.id;
+    });
+	};
+	
+	const edit= (id)=>{
+      let newedititem = items.find((elem)=>{
+        return elem.id ===id;
+			})
+			setToggle(false);
+			setInputs(newedititem.name);
+			setIsEditItem(id);
+	}
+
+  return (
+    <div id="main">
+      <textarea id="task" value={inputs} onChange={heandlechange} />
+      <button id="btn" onClick={additem}>
+        Add
+      </button>
+      <ul>
+        {items.map((elem) => {
+          return (
+            <li className="list" key={elem.id}>
+              {elem.name}
+            </li>
+            <button onClick={()=>(del(elem.id)}>Delete</button>
+						<button onClick={()=>edit(elem.id)}>Edit</button>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
 
 export default App;
